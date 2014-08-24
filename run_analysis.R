40da6b07b82b7424b4efb4486e6cffdfd94a0643
MelanieIStefan <melanie.stefan@cantab.net>
2014-08-24 13:34:28 -0400

    extracted only data with mean or standard deviation

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,8 @@
-# Merge the training and the test sets to create one data set.
+## load necessary libraries
+
+library(stringr)
+
+## Merge the training and the test sets to create one data set.
 
 # Read in training and test data sets and labels
 
@@ -8,10 +12,31 @@ trainLabels <- read.table("train/y_train.txt")
 testData <- read.table("test/X_test.txt")
 testLabels <- read.table("test/y_test.txt")
 
+# Merge test and train data
+
 allData <- rbind(trainData,testData)
 allLabels <- rbind(trainLabels,testLabels)
 
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Remove datasets no longer used
+
+rm(testData)
+rm(testLabels)
+rm(trainData)
+rm(trainLabels)
+
+# Extract only the measurements on the mean and standard deviation for each measurement. 
+
+# Get columns with mean or std in their names only
+
+features <- read.table("features.txt")
+
+mean_std <- features[str_detect(features[ ,2], "mean") | str_detect(features[,2], "std"), ]
+dim(mean_std)
+
+allData=allData[ ,mean_std[ ,1]]
+
+
+
 
 # Uses descriptive activity names to name the activities in the data set
 
